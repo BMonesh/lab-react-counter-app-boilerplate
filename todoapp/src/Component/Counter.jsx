b53b010a84f772044966a20b7a6ad1088faecfee
BMonesh <monesh.b@kalvium.community>
2024-01-24 14:37:24 +0530

    heaan

---
+++ todoapp/src/Component/Counter.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+class Counter extends React.Component{
+
+constructor(){
+  super();
+  this.state = {
+    count:0
+  }
+  
+}
+
+
+  render(){
+    
+    let incrementCount = () => {
+      this.setState(prevState => {
+        return {count: prevState.count + 1}
+      })
+    }
+
+    let decrementCount = () => {
+      this.setState(prevState => {
+        return {count: prevState.count - 1}
+      })
+    }
+
+    let resetCounter = () => {
+      this.setState({count : 0})
+    }
+  
+
+    return(
+
+      <div>
+        <h1>
+          Counter App
+        </h1>
+        <p>{this.state.count}</p>
+        <div>
+          <button onClick={incrementCount}>+</button>
+          <button onClick={decrementCount}>-</button>
+          <button onClick={resetCounter}>Reset</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter;
